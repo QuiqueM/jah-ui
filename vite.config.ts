382f5a535b12b4d6e5d972dc18a13e596d3b082c
Enrique Martinez <m_r_enrique@outlook.com>
2026-05-23 00:17:54 -0600

    fix: changes to the build

--- vite.config.ts
+++ vite.config.ts
@@ -18,20 +18,34 @@ export default defineConfig({
     }),
   ],
   build: {
+    emptyOutDir: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'JahUI',
-      fileName: 'jah-ui',
+      // 💡 Quitamos los formatos de aquí arriba para manejarlos abajo de forma estricta
     },
     rollupOptions: {
       external: ['vue', 'chart.js', 'vue-chartjs'],
-      output: {
-        globals: { 
-          vue: 'Vue',
-          'chart.js': 'Chart',
-          'vue-chartjs': 'VueChartjs'
+      // 💡 Configuramos un array de outputs para darle reglas independientes a cada formato
+      output: [
+        {
+          format: 'es',
+          inlineDynamicImports: true, // 💥 Fuerza a meter TODO en el archivo ESM
+          entryFileNames: 'jah-ui.es.js',
+          preserveModules: false      // Evita explícitamente que preserve los archivos individuales
         },
-      },
+        {
+          format: 'umd',
+          inlineDynamicImports: true, // 💥 Fuerza a meter TODO en el archivo UMD
+          entryFileNames: 'jah-ui.umd.js',
+          name: 'JahUI',
+          globals: {
+            vue: 'Vue',
+            'chart.js': 'Chart',
+            'vue-chartjs': 'VueChartjs'
+          }
+        }
+      ]
     },
   },
   resolve: {
